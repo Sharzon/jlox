a3508dcfe21e27d89d21e5c1aae63b26526afc6b
Ilia Eliseev <sharzon.soft@gmail.com>
2023-01-07 11:43:18 +0100

    Change print to reverse Polish notation

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -30,13 +30,24 @@ class AstPrinter implements Expr.Visitor<String> {
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("(").append(name);
         for (Expr expr : exprs) {
-            builder.append(" ");
             builder.append(expr.accept(this));
+            builder.append(" ");
         }
-        builder.append(")");
+        builder.append(name);
 
         return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
     }
 }
